fbf748e1bc3235d9a5fb930b29afbb5cc82176d8
surabhisutar <surabhisutar95@gmail.com>
2022-04-10 01:56:45 +0530

    app

--- App.py
+++ App.py
@@ -1,66 +1,14 @@
-import pandas as pd
-from scipy.sparse import csr_matrix
-from sklearn.neighbors import NearestNeighbors
-import matplotlib.pyplot as plt
+from flask import Flask,request,jsonify
+from flask_cors import CORS
+import serv
 
+app = Flask(__name__)
+CORS(app) 
+        
+@app.route('/movie', methods=['GET'])
+def recommend_movies():
+    res = serv.get_movie_recommendation(request.args.get('title'))
+    return jsonify(res)
 
-movies = pd.read_csv("movies copy.csv")
-ratings = pd.read_csv("ratings copy.csv")
-
-final_dataset = ratings.pivot(index='movieId',columns='userId',values='rating')
-final_dataset.fillna(0,inplace=True)
-
-no_user_voted = ratings.groupby('movieId')['rating'].agg('count')
-no_movies_voted = ratings.groupby('userId')['rating'].agg('count')
-
-f,ax = plt.subplots(1,1,figsize=(16,4))
-# ratings['rating'].plot(kind='hist')
-plt.scatter(no_user_voted.index,no_user_voted,color='mediumseagreen')
-plt.axhline(y=10,color='r')
-plt.xlabel('MovieId')
-plt.ylabel('No. of users voted')
-#plt.show()
-
-final_dataset = final_dataset.loc[no_user_voted[no_user_voted > 10].index,:]
-
-f,ax = plt.subplots(1,1,figsize=(16,4))
-plt.scatter(no_movies_voted.index,no_movies_voted,color='mediumseagreen')
-plt.axhline(y=50,color='r')
-plt.xlabel('UserId')
-plt.ylabel('No. of votes by user')
-#plt.show()
-
-final_dataset=final_dataset.loc[:,no_movies_voted[no_movies_voted > 50].index]
-
-csr_data = csr_matrix(final_dataset.values)
-final_dataset.reset_index(inplace=True)
-
-knn = NearestNeighbors(metric='cosine', algorithm='brute', n_neighbors=20, n_jobs=-1)
-knn.fit(csr_data)
-
-def get_movie_recommendation(movie_name):
-    n_movies_to_reccomend = 10
-    movie_list = movies[movies['title'].str.contains(movie_name)]
-    print(movie_list,len(movie_list))
-    if len(movie_list):
-        movie_idx= movie_list.iloc[0]['movieId']
-        print(movie_idx)
-        movie_idx = final_dataset[final_dataset['movieId'] == movie_idx].index[0]
-        print(final_dataset)
-        #final_dataset.to_csv("final_dataset.csv")
-        print(movie_idx)
-        distances , indices = knn.kneighbors(csr_data[movie_idx],n_neighbors=n_movies_to_reccomend+1)
-        print(distances)
-        print(indices)
-        rec_movie_indices = sorted(list(zip(indices.squeeze().tolist(),distances.squeeze().tolist())),key=lambda x: x[1])[:0:-1]
-        recommend_frame = []
-        for val in rec_movie_indices:
-            movie_idx = final_dataset.iloc[val[0]]['movieId']
-            idx = movies[movies['movieId'] == movie_idx].index
-            recommend_frame.append({'Title':movies.iloc[idx]['title'].values[0],'Distance':val[1]})
-        df = pd.DataFrame(recommend_frame,index=range(1,n_movies_to_reccomend+1))
-        return df.to_dict('records')
-    else:
-        return "No movies found. Please check your input"
-
-#get_movie_recommendation(Movie_Name)
+if __name__=='__main__':
+    app.run(port = 5000, debug = True)
